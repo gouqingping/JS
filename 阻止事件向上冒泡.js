8606c1f0ab4846d500943bad6ae768ab50d96582
Pat <gouqingping@yahoo.com>
2016-03-30 10:48:38 +0800

    Update 阻止事件向上冒泡.js

--- 阻止事件向上冒泡.js
+++ 阻止事件向上冒泡.js
@@ -1,12 +1,19 @@
 $(function(){
-	$(".search").click(function (event){
-		$('.search_results').show();//显示
-		$(document).click(function(){
-			$('.search_results').hide();//影藏
-			});
+	// 定义触发事件原属
+	search       = $(".search");
+	searchShow   = $('.search_results');
+	
+	search.click(function (event){//点击触发原素，触发事件
+	
+		searchShow.show();//事件显示
+		$(document).click(function(){//点击区域外部任意位置隐藏
+			searchShow.hide();
+		});
+		event.stopPropagation();//阻止事件向上冒泡
+		
+	});
+	searchShow.click(function (event){
 		event.stopPropagation();//阻止事件向上冒泡
 	});
-	$('.search_results').click(function (event){
-		event.stopPropagation();//阻止事件向上冒泡
-	});
+	
 });
